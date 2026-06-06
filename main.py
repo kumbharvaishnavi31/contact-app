7a059dc1df097712574bcabcdc8abb433257b189
Vaishnavi Maruti Kumbhar <kumbharvaishnavi3125@gmail.com>
2026-06-06 12:20:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import requests
+import streamlit as st
+import pandas as pd
+st.set_page_config(
+    page_title = " Contact App Project",
+    page_icon = "📞"
+)
+
+
+st.title("Contact App")
+st.write('Click on the button below to fetch the user data')
+
+if st.button("Fetch Contacts"):
+    response = requests.get("https://jsonplaceholder.typicode.com/users")
+    users = response.json()
+    
+    users_data = []
+    for user in users:
+        users_data.append({
+            "id" : user["id"],
+            "name" : user["name"],
+            "email" : user["email"],
+            "website" : user["website"]
+        })
+
+    df = pd.DataFrame(users_data)
+    
+    st.subheader("Contact Information")
+    st.dataframe(df)
+
+
+# response = requests.get("https://jsonplaceholder.typicode.com/users")
+
+# users = response.json()
+# # [200] - everything is ok (state code)
+# print(type(users)) 
+
+# for user in users:
+#     print(user["id"])
+#     print(user["name"])
+#     print(user["email"])
+#     print("------------------")
+    
+    
+    
+# streamlit run main.py
